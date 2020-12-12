5348cea60221cf2fd4d4a1430518cdd4fc963dae
AmarAhuja <70715279+AmarAhuja@users.noreply.github.com>
2020-12-13 01:25:00 +0530

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+defined('DB_SERVER') ? null : define("DB_SERVER", "localhost:3307");
+defined('DB_NAME')   ? null : define("DB_NAME", "Tradly"); // Name of the database
+defined('DB_USER')   ? null : define("DB_USER", "root"); // User with database access
+defined('DB_PASS')   ? null : define("DB_PASS", ""); // Password of the user
+
+?>
